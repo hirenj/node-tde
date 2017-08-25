0f38563127349210987d1f4179f7ecb54ec48a69
Hiren Joshi <hirenj@gmail.com>
2017-08-25 14:13:55 +0200

    Add in serialisation of TDS files too

--- test.js
+++ test.js
@@ -6,5 +6,6 @@ const fs = require('fs');
 let output = fs.createWriteStream('output.tde');
 let objwriter = new writer(output);
 let dataframe = [{x: 1, y: 'a', z: true }];
-let env_done = objwriter.environment({'frame' : dataframe },{'frame' : { 'type': 'dataframe', 'keys' : ['x','y','z'], 'types' : ['int','string','logical'] }});
+let typedef = { 'type': 'dataframe', 'keys' : ['x','y','z'], 'types' : ['int','string','logical'] }
+let env_done = objwriter.environment({'frame' : dataframe },{'frame' : typedef });
 env_done.then( () => objwriter.finish() );
